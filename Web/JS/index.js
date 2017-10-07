d0614c3d28c546c971a555d24163bc5fa594e99c
sornichou <sornhulsen@outlook.com>
2017-10-07 16:00:28 +0200

    change css

--- Web/JS/index.js
+++ Web/JS/index.js
@@ -7,8 +7,8 @@ $(function() {
 	// quand on clique sur les boutons du menu de navigation
     $('nav span').click(function() {
         // modifie les bordures du menu
-        $('nav span').css('border-radius', '0px').css('background-color', '#f4f4f4');
-        $(this).css('border-radius', '10px 1px').css('background-color', '#9ec1a6');
+        $('nav span').css('background-color', '#f4f4f4');
+        $(this).css('background-color', '#9ec1a6');
 
         // assigne une valeur qui servira d'identifiant
         var up = capitalize($(this).attr('id'));
@@ -16,9 +16,9 @@ $(function() {
 
         // si on clique sur un autre bouton, efface le texte courant et affiche le texte voulu
         if(id != selectedSct) {
-        	$('#' + selectedSct).slideUp('fast');
+        	$('#' + selectedSct).fadeOut('fast');
         	selectedSct = id;
-			$('#' + id).delay(300).slideDown('fast');
+			$('#' + id).delay(300).fadeIn('slow');
         }
     });
 
